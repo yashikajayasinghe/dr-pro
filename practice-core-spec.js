84da2576d0578b498dd7eb0feafadc64b13b1de5
yjayasinghe <sam@example.comyashika.jayasinghe@trademe.co.nz>
2020-04-11 07:44:16 +1200

    spec file to practice core

---
+++ practice-core-spec.js
@@ -0,0 +1,13 @@
+/**
+ * All the tests are included in the spec file.
+ * Spec file uses Jasmine synatx ('describe' for the 'test suite' and 'it' for the 'test case') https://jasmine.github.io/
+ * Protractor code is placed in the 'it' function
+ * ref https://www.protractortest.org/#/tutorial
+ * @Author: YashJ
+ */
+describe('Learn core api', function(){
+it('practice test case', function(){
+    
+})
+
+})
