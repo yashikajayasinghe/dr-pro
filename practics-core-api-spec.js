d624cbbeb3909f0933442cc09fd3066365982ac0
yjayasinghe <sam@example.comyashika.jayasinghe@trademe.co.nz>
2020-04-11 11:42:29 +1200

    -practice

---
+++ practics-core-api-spec.js
@@ -0,0 +1,14 @@
+describe('Learning protractor core API', function(){
+	
+    it('test-chainlocators',function(){
+
+        
+    });
+    it('test-all-method',function(){
+        
+    });
+    it('test-list-dropdown',function(){
+        
+    });
+    
+});
